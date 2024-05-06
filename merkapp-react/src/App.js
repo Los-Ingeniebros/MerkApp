8fa80e8a44ca71650eabe91c14e3b0d9090f833b
EduardoHdzF <90948381+EduardoHdzF@users.noreply.github.com>
2024-05-05 18:38:43 -0600

    pruebas con react

--- merkapp-react/src/App.js
+++ merkapp-react/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logoo.png';
+import React from 'react';
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+  emoji: "😀",
+  name: "grinning face"
+  },
+  {
+  emoji: "🎉",
+  name: "party popper"
+  },
+  {
+  emoji: "💃",
+  name: "woman dancing"
+  }
+  ];
+  
 function App() {
+  const greeting = "greeting";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola, chifu
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='container'>
+    <h1 id={greeting}>Hola, mamon</h1>
+    <p>Esto es un ejemplo de como escribir las cosas en React</p>
+    <ul>
+      <li>
+        <button onClick={displayEmojiName}>
+          <span role="img" aria-label="grinning face" id="grinning face"/>
+          a
+        </button>
+      </li>
+      <li>
+      <button onClick={displayEmojiName}>
+      <span role="img" aria-label="party popper" id="party popper" />
+      b
+      </button>
+      </li>
+      <li>
+      <button onClick={displayEmojiName}>
+      <span role="img" aria-label="woman dancing" id="woman dancing" />
+      c
+      </button>
+      </li>
+    </ul>
+  </div>
+
+)
 }
 
 export default App;
