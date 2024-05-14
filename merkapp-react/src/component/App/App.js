1c9cadfa129ce2e4e29a2b52c48bc6041d12272b
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-05-14 12:25:39 -0600

    Actualizando App

--- merkapp-react/src/component/App/App.js
+++ merkapp-react/src/component/App/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import LogInForm from '../Login/Login';
+import HomeVendedor from './HomeVendedor';
+import HomeComprador from './HomeComprador';
+import Eliminar from './EliminarVentas';
+import AgregarOpinion from './AgregarOpinion';
+
 
 import logo from '../../imagenes/MerkAppSinFondo.png';
 
@@ -24,8 +32,13 @@ function App() {
 
   // const greeting = "greeting";
 
+  const [user, setUser] = useState('');
+  const [users, setUsers] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();  
+
   async function ingresar (name) {
-    console.log(name);
+    console.log(name);        
     const response = await fetch('http://127.0.0.1:5000/login', {
       method:'POST',
       body: JSON.stringify(name),
@@ -34,15 +47,93 @@ function App() {
       }
     });
     const data = await response.json();
-    console.log(data);
+    console.log(data); 
     if (data.error !== undefined) {
       alert("ERROR! " + data.error);
     } else {
-      alert("Usuario encontrado!");
+      alert("Usuario encontrado!");   
+      console.log(data['nombre'])
+      const usr = [data['modo'], data['nombre'], data['correo'], data['contrasenia']]
+      setUser(usr);  
+      console.log(data['modo']);
+      if(data['modo'] == 'Vendedor'){
+        navigate('/vendedor');      
+      } else if(data['modo'] == 'Comprador'){
+        navigate('/comprador');      
+      }
+    }     
+  };
+
+  async function recuperar () {
+    const response = await fetch('http://127.0.0.1:5000/recuperar', {
+      method:'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type':'application/json'
+      }
+    });      
+    const data = await response.json();    
+    console.log(data);
+    setUsers(data['dic']);    
+  };  
+
+  useEffect(() => {            
+    if (location.pathname === '/') {            
+      Cookies.set('user', user);
+      var almacenadoUser = Cookies.get('user');
+      if (almacenadoUser) {
+        //setUser(almacenadoUser);
+        almacenadoUser = almacenadoUser.split(",");           
+        console.log('Nombre de usuario recuperado:', almacenadoUser);
+        //Cookies.remove('user');
+        if (almacenadoUser[0] == 'Vendedor'){
+          navigate('/vendedor');
+        } else if (almacenadoUser[0] == 'Comprador'){
+          navigate('/comprador');
+        }         
+      } else {
+        console.log('No se encontró ningún nombre de usuario almacenado en los cookies.');
+      }      
+      //navigate('/home');
+    } else if (location.pathname === '/vendedor') {            
+      var almacenadoUser = Cookies.get('user');
+      if (almacenadoUser) {     
+        almacenadoUser = almacenadoUser.split(",");   
+        setUser(almacenadoUser);
+        console.log('Nombre de usuario recuperado:', almacenadoUser);  
+        if (almacenadoUser[0] == 'Comprador'){
+          navigate('/comprador');
+        }       
+      } else {        
+        Cookies.set('user', user);
+        //Cookies.remove('user');
+        //navigate('/');        
+        console.log('No se encontró ningún nombre de usuario almacenado en los cookies.');
+      }
+    } else if (location.pathname === '/comprador') {            
+      var almacenadoUser = Cookies.get('user');
+      if (almacenadoUser) {     
+        almacenadoUser = almacenadoUser.split(",");   
+        setUser(almacenadoUser);
+        console.log('Nombre de usuario recuperado:', almacenadoUser); 
+        if (almacenadoUser[0] == 'Vendedor'){
+          navigate('/vendedor');
+        }        
+      } else {        
+        Cookies.set('user', user);
+        //Cookies.remove('user');
+        //navigate('/');        
+        console.log('No se encontró ningún nombre de usuario almacenado en los cookies.');
+      }
+    } else if (location.pathname === '/login') {            
+      Cookies.remove('user');
+    } else if (location.pathname === '/vendedor/eliminar') {
+      recuperar();      
     }
-  }
+  }, [location.pathname]);
 
   return (
+    /*
   <div className='login'>
     <div class="area" >
       <ul class="circles">
@@ -58,7 +149,8 @@ function App() {
         <li></li>
       </ul>
       <div className='container'>
-        {/* <h1 id={greeting}>Hola, mamon</h1> */}
+        {/* <h1 id={greeting}>Hola, mamon</h1> */
+      /*}
         {/* 
         <p>Esto es un ejemplo de como escribir las cosas en React</p>
         <ul>
@@ -80,7 +172,8 @@ function App() {
               
               ))
           }
-        </ul> */}
+        </ul> */
+      /*}
         <header className="App-header" id="app-header">
           <span class="logo-log">
             <img src={logo} alt="MerkApp's logo"/>
@@ -91,7 +184,21 @@ function App() {
       </div>
     </div >
   </div>
-)
+  */
+  <div className="App">
+    <header className="App-header">  
+      <h1>MerkApp</h1>                 
+      <Routes>
+        <Route path="/" element={<LogInForm onSaveName={ingresar}/>} />  
+        <Route path="/vendedor" element={HomeVendedor(user)} />
+        <Route path="/comprador" element={HomeComprador(user)} />
+        <Route path="/vendedor/eliminar" element={Eliminar(users)} />  
+        <Route path="/comprador/agregar" element={AgregarOpinion(user)} />  
+        <Route path="/login" element={<LogInForm onSaveName={ingresar}/>} />  
+      </Routes>        
+    </header>      
+  </div>
+  );
 }
 
 export default App;
