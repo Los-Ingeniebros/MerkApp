595c65c8f1ebff8d0ed84cd92cd3dfde3ea5a0cf
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-05-14 14:51:15 -0600

    Actualizando app

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,20 +1,15 @@
-from flask import Flask, redirect, render_template, url_for, request, flash, session, Blueprint
-
-import json
-
 from flask import Flask, redirect, render_template, url_for, request, flash, session, Response
 from flask_cors import CORS
-
 from alchemyClasses import db
 from controller.catalogue import catalogue
 from alchemyClasses.Comprador import Comprador
 from alchemyClasses.Vendedor import Vendedor
 from alchemyClasses.Producto import Producto
 from alchemyClasses.Comprar import Comprar
+import json
 
 app = Flask(__name__)
 app.register_blueprint(catalogue)
-#app.register_blueprint(usuario_blueprint)
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ing:Developer123!@localhost:3306/base_merkaap'
 db.init_app(app)
@@ -70,24 +65,10 @@ def recuperarVentas():
         contrasenia = request.json[3]        
         vendedor = Vendedor.query.filter(Vendedor.correo == correo, Vendedor.contrasenia == contrasenia).first()
         diccionario = {}
-        #lista = []
         for registro in Producto.query.filter(Producto.idVendedor == vendedor.idVendedor):
-            #usuario = {registro.idUsuario:{ "nombre":f"{registro.nombre}", "contrasenia":f"{registro.contrasenia}"}}            
             venta = {registro.idProducto:[registro.nombre, registro.stock, registro.precio, registro.descripcion]}
-            #usuario = [registro.idUsuario, registro.nombre, registro.contrasenia]
-            #lista.append(usuario)
             diccionario.update(venta)    
         return json.dumps({'dic': diccionario})
-        #exit(0)
-        #lista = request.json['lista']
-        
-        #encontrado = Usuario.query.filter(Usuario.nombre == nombre, Usuario.contrasenia == contrasenia).first()
-      
-        #if encontrado: #Ustedes van a tener que cambiar esto, por una validación con la DB.
-        #    session['user_id'] = nombre #definición de cookie de sesión.            
-        #    return json.dumps({'nombre': nombre, 'contrasenia': contrasenia})
-        #flash('Invalid username or password')
-        #return json.dumps({'error':'Invalid username or password'})
 
 
 @app.route('/eliminarVentas', methods=['GET', 'POST', 'OPTIONS'])
@@ -102,6 +83,9 @@ def eliminarVentas():
         lista_de_id = request.json
         for id in lista_de_id:
             venta = Producto.query.filter(Producto.idProducto == id).first()
+            for registro in Comprar.query.filter(Comprar.idProducto == id).all():
+                db.session.delete(registro)
+                db.session.commit()
             db.session.delete(venta)
             db.session.commit()
         return json.dumps({'listo':'usuario'})
@@ -110,7 +94,6 @@ def eliminarVentas():
 def agregarOpinion(): 
     if request.method == 'OPTIONS':                   
         res = Response()        
-        #res.headers['X-Content-Type-Options'] = '*'
         return res
     elif request.method == 'POST':        
         usuario = request.json[0]
@@ -121,10 +104,19 @@ def agregarOpinion():
             id += 1
 
         comprador = Comprador.query.filter(Comprador.correo == usuario[2], Comprador.contrasenia == usuario[3]).first()
-        # El idProducto no cambia por el momento
-        compra = Comprar(id, comprador.idComprador, 8, opinion[1], opinion[0])
-        db.session.add(compra)
-        db.session.commit()        
+        # El idProducto no cambia por el momento, y se debe de poner un valor que exista en la base de datos
+        if opinion[0] != '' and opinion[1] != '':
+            compra = Comprar(id, comprador.idComprador, 10, opinion[1], opinion[0])
+            db.session.add(compra)
+            db.session.commit()        
+        elif opinion[0] == '' and opinion[1] != '':
+            compra = Comprar(id, comprador.idComprador, 10, opinion[1], 0)
+            db.session.add(compra)
+            db.session.commit()        
+        elif opinion[0] != '' and opinion[1] == '':
+            compra = Comprar(id, comprador.idComprador, 10, opinion[1], opinion[0])
+            db.session.add(compra)
+            db.session.commit()                
         return json.dumps({'listo':'usuario'})
 
 @app.route('/logout')
