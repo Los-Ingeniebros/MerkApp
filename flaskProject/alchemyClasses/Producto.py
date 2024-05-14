475370958ed204271b5e3b873b77ed7a4a7d0b41
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-05-14 09:40:55 -0600

    Actualizando Producto

--- flaskProject/alchemyClasses/Producto.py
+++ flaskProject/alchemyClasses/Producto.py
@@ -6,21 +6,27 @@ class Producto(db.Model):
 
     __tablename__ = 'producto'
     idProducto = Column(Integer, nullable=False, primary_key=True)
-    foto = Column(BLOB, nullable=False)
+    idVendedor = Column(Integer, nullable=False)
+    idCategoria = Column(Integer, nullable=False)
+    nombre = Column(String(200), nullable=False)
     descripcion = Column(String(200), nullable=False)
     precio = Column(Integer, nullable=False)
-    telefono = Column(String(10), foreign_key=True)
-    correo = Column(String(500), foreign_key=True)
+    stock = Column(Integer, nullable=False)
+    fotoDeProducto = Column(BLOB)        
+    #telefono = Column(String(10), foreign_key=True)
+    #correo = Column(String(500), foreign_key=True)
 
-    def __init__(self, idProducto, foto, descripcion, precio, telefono, correo):
+    def __init__(self, idProducto, idVendedor, idCategoria, nombre, precio, stock, fotoDeProducto):
         self.idProducto = idProducto
-        self.foto = foto
-        self.descripcion = descripcion
+        self.idVendedor = idVendedor
+        self.idCategoria = idCategoria
+        self.nombre = nombre
         self.precio = precio
-        self.telefono = telefono
-        self.correo = correo
+        self.stock = stock
+        self.fotoDeProducto = fotoDeProducto
 
     #Sugerencia, después poner las funciones en los modelos de tal manera que en app.py no tengamos eso y solo lo mandemos a llamar
+    '''
     def session(self, idProducto, precio):
         if session.get('user_id') != None:
             return render_template('login.html', user=session['user_id'])
@@ -38,6 +44,7 @@ class Producto(db.Model):
                     session['user_id'] = name #definición de cookie de sesión.
                     return render_template('login.html', user=name)
         return redirect(url_for('login'))
+    '''    
 
     # def __str__(self):
     #     return f'Nombre completo: {self.nombre} {self.apellidoPat} {self.apellidoMat}\n - Id: {self.idComprador}\n - Correo: {self.correo}\n - Contraseña: {self.contrasena}\n - Teléfono: {self.telefono}\n'
